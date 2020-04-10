3ce27f28608e5b6ec7696da00569fa748a434744
six <1293244060@qq.com>
2020-04-10 21:39:41 +0800

    verbose3

--- PortScanClass.py
+++ PortScanClass.py
@@ -22,7 +22,7 @@ class PortScanClass(object):
                 print("[+] %s %d \033[91m Closed \033[0m" % (self.host,dport))
             elif resp.haslayer("TCP"):
                 if resp["TCP"].flags=="SA":  # 0x12
-                    send_rst = sr(IP(dst=self.host)/TCP(dport=dport,flags="AR"),timeout=10,,verbose=0)
+                    send_rst = sr(IP(dst=self.host)/TCP(dport=dport,flags="AR"),timeout=10,verbose=0)
                     print("[+] %s %d \033[92m open \033[0m" % (self.host,dport))
                 elif resp["TCP"].flags=="RA":  # 0x14
                     print("[+] %s %d \033[91m Closed \033[0m" % (self.host,dport))
